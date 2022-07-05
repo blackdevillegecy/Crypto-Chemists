ca3da675ae47485c896738c867e8fcddd2ed50e8
Ayush Gautam <gautamayush953@gmail.com>
2022-07-05 23:09:31 +0530

    repo updated

---
+++ crypto-chemists/src/App.js
@@ -0,0 +1,39 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './comps/Navbar.js';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <img src="" alt="" />
+      <div id='molecule'>
+        <div className='circle'></div>
+        <div id="line1"></div>
+        <div className='circle' id='medium-c'></div>
+        <div id="line2"></div>
+        <div className='circle' id='small-c'></div>
+        <div id="line3"></div>
+        <div className="circle" id="semi-large-c"></div>
+      </div>
+
+
+      <button type='button' 
+      onMouseEnter ={() => {
+        document.getElementById('arrow-icon').style.color = 'yellow'
+      }}
+      onMouseLeave = {() => {
+        document.getElementById('arrow-icon').style.color = 'white'
+      }}
+      >Take a Tour <ArrowForwardIcon id='arrow-icon' /></button>
+
+    <div id='img-nft'></div>
+
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ crypto-chemists/src/comps/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div>
+        <div id="navbar">
+            <div className="nav-items">Buy</div>
+            <div className="nav-items">Listings</div>
+            <div className="nav-items">Sales</div>
+            <div className="nav-items">Roadmap</div>
+            <div className="nav-items">Team</div>
+            <div className="nav-items">Contact</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
